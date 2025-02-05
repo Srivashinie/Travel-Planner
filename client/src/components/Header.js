23294a68b4d578d39e5859c2c6a0e6fbb31a4bf4
satvika2020 <satvika.sai9@gmail.com>
2025-02-05 11:55:36 -0800

    Adding basic header component,itin page and home pages and style changes with welcome messages and react bootstrap packages

---
+++ client/src/components/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../style.css";
+
+const Header = () => {
+  return (
+    <Navbar
+      expand="lg"
+      style={{
+        background: "#2C3E50",
+        color: "white",
+      }}
+    >
+      <Container>
+        <Navbar.Brand href="/" style={{ color: "white", fontWeight: "bold" }}>
+          Travel Planner
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              as={Link}
+              to="/"
+              className="nav-link"
+              style={{ color: "white", fontWeight: "bold" }}
+            >
+              Home
+            </Nav.Link>
+
+            <Nav.Link
+              as={Link}
+              to="/itin"
+              className="nav-link"
+              style={{ color: "white", fontWeight: "bold" }}
+            >
+              Itin
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ client/src/pages/Itin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Itin = () => {
+  return (
+    <div>
+      <h1>This is the Itinerary Search page</h1>
+    </div>
+  );
+};
+
+export default Itin;
