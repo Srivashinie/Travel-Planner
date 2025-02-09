4e661d6db0a1ded03efdc31e76f7c6218532fe72
Srivashinie <srivashinie24@gmail.com>
2025-02-09 11:13:25 -0800

    Updated Search Restaurant Feature

--- client/src/pages/Restaurants.js
+++ client/src/pages/Restaurants.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import RestaurantSearch from "../components/RestaurantSearch";
+import RestaurantList from "../components/RestaurantList";
 
 const Restaurants = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  const fetchRestaurants = async (location, category, price) => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:5000/api/restaurants/restaurants",
+        {
+          params: { location, category, price },
+        }
+      );
+      setRestaurants(response.data);
+    } catch (error) {
+      console.error("Error fetching restaurants:", error);
+    }
+  };
+
   return (
-    <div>
-      <h1>This is the Restaurants Search page</h1>
+    <div className="restaurants-container">
+      <RestaurantSearch onSearch={fetchRestaurants} />
+      <RestaurantList restaurants={restaurants} />
     </div>
   );
 };
--- server/app.py
+++ server/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.itinerary import itinerary_bp
+from routes.restaurants import restaurants_bp 
 
 app = Flask(__name__)
 CORS(app)
 
 # Register blueprints for different routes
 app.register_blueprint(itinerary_bp, url_prefix='/api/itinerary')
+app.register_blueprint(restaurants_bp, url_prefix='/api/restaurants')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
