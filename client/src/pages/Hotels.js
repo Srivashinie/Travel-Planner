42fb6c3d9a3306f53ab12dff33a9e55851e14440
Neha Khawle <nehak@pdx.edu>
2025-02-23 01:23:15 -0800

    Added Check-in check-out feature for hotel page

--- client/src/pages/Hotels.js
+++ client/src/pages/Hotels.js
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 import "../style.css";
+import default_hotel_img from "./hotel-image.jpg";
 
 const Hotels = () => {
   const [hotels, setHotels] = useState([]);
   const [destination, setDestination] = useState("");
   const [loading, setLoading] = useState(true);
   const [budget, setBudget] = useState("all");
+  const [checkInDate, setCheckInDate] = useState("");
+  const [checkOutDate, setCheckOutDate] = useState("");
 
   // Function to fetch hotels from Flask backend
   const searchHotels = async () => {
-    if (!destination) {
-      alert("Please enter a destination");
+    if (!destination || !checkInDate || !checkOutDate) {
+      alert("Please enter all the fields");
       return;
     }
 
     try {
       // Send GET request to Flask backend server
       const response = await fetch(
-        `http://localhost:5000/api/hotel/generate?destination=${destination}`
+        `http://localhost:5000/api/hotel/generate?destination=${destination}&check_in_date=${checkInDate}&check_out_date=${checkOutDate}`
       );
       const data = await response.json();
 
@@ -38,24 +41,57 @@ const Hotels = () => {
   const budgetFilter = (hotel) => {
     const price =
       parseFloat(hotel.total_rate.lowest.replace(/[^0-9.]/g, "")) || 0;
-    console.log(`Hotel: ${hotel.name}, Price: ${price}, Budget: ${budget}`);
 
     if (budget === "cheap") return price < 100;
     if (budget === "normal") return price >= 100 && price <= 180;
     if (budget === "high") return price > 180;
 
-    return true; // Show all if no filter applied
+    return true;
+  };
+
+  const checkImageExists = (url) => {
+    return new Promise((resolve) => {
+      if (!url || url.trim() === "") {
+        resolve(false);
+        return;
+      }
+      const img = new Image();
+      img.src = url;
+      img.onload = () => resolve(true);
+      img.onerror = () => resolve(false);
+    });
+  };
+
+  const getValidImage = (hotel) => {
+    if (!hotel.images || hotel.images.length === 0) {
+      return default_hotel_img;
+    }
+
+    for (const img of hotel.images) {
+      if (img?.thumbnail?.trim()) {
+        const isValid = checkImageExists(img.thumbnail);
+        if (isValid) return img.thumbnail;
+      }
+    }
+
+    return default_hotel_img;
   };
 
   return (
     <div className="hotels-container">
       <div className="form-box">
-        <h2>
+        <h2 style={{ fontSize: "30px" }}>
           <strong>EXPLORE HOTELS</strong>
         </h2>
         <div className="search-box">
-          <lebel style={{ fontWeight: "bold", fontSize: "17px" }}>
-            Destination:{" "}
+          <lebel
+            style={{
+              fontWeight: "bold",
+              fontSize: "17px",
+              fontFamily: "Arial, sans-serif",
+            }}
+          >
+            Destination:
           </lebel>
           <input
             type="text"
@@ -64,26 +100,88 @@ const Hotels = () => {
             value={destination}
             onChange={(e) => setDestination(e.target.value)}
             style={{
-              marginRight: "15px",
+              margin: "10px",
               padding: "8px",
-              width: "250px",
+              width: "660px",
+              border: "1px solid lightgray",
+              fontFamily: "Arial, sans-serif",
+              fontSize: "16px",
             }}
           />
-          {/* Budget filter dropdown */}
-          <div className="filter-box">
-            <label htmlFor="budget">
-              <stong>Budget: </stong>{" "}
-            </label>
-            <select
-              id="budget"
-              value={budget}
-              onChange={(e) => setBudget(e.target.value)}
-            >
-              <option value="all">All</option>
-              <option value="cheap">Cheap (Below $100)</option>
-              <option value="normal">Normal ($100 - $180)</option>
-              <option value="high">High (Above $180)</option>
-            </select>
+
+          {/* Check-in Date */}
+          <div className="search-fields">
+            <div>
+              <label
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "17px",
+                  fontFamily: "Arial, sans-serif",
+                }}
+              >
+                Check-in:
+              </label>
+              <input
+                type="date"
+                value={checkInDate}
+                onChange={(e) => setCheckInDate(e.target.value)}
+                style={{
+                  margin: "5px",
+                  padding: "5px",
+                  width: "150px",
+                  fontSize: "16px",
+                  border: "1px solid lightgray",
+                  height: "40px",
+                  fontFamily: "Arial, sans-serif",
+                  color: "black",
+                }}
+              />
+            </div>
+
+            {/* Check-out Date */}
+            <div>
+              <label
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "17px",
+                  fontFamily: "Arial, sans-serif",
+                }}
+              >
+                Check-out:
+              </label>
+              <input
+                type="date"
+                value={checkOutDate}
+                onChange={(e) => setCheckOutDate(e.target.value)}
+                style={{
+                  margin: "5px",
+                  padding: "8px",
+                  width: "150px",
+                  fontSize: "17px",
+                  border: "1px solid lightgray",
+                  height: "40px",
+                  color: "black",
+                }}
+              />
+            </div>
+
+            {/* Budget filter dropdown */}
+            <div className="filter-box">
+              <label htmlFor="budget">
+                <stong>Budget: </stong>{" "}
+              </label>
+              <select
+                id="budget"
+                value={budget}
+                onChange={(e) => setBudget(e.target.value)}
+                style={{ border: "1px solid lightgray" }}
+              >
+                <option value="all">All</option>
+                <option value="cheap">Cheap (Below $100)</option>
+                <option value="normal">Normal ($100 - $180)</option>
+                <option value="high">High (Above $180)</option>
+              </select>
+            </div>
           </div>
         </div>
         <button onClick={searchHotels}>Search Hotels</button>
@@ -99,13 +197,7 @@ const Hotels = () => {
           hotels.filter(budgetFilter).map((hotel, index) => (
             <div key={index} className="hotel-card">
               <img
-                src={
-                  hotel.images?.[0]?.thumbnail ||
-                  hotel.images?.[1]?.thumbnail ||
-                  hotel.images?.[2]?.thumbnail ||
-                  hotel.images?.[3]?.thumbnail ||
-                  "default-hotel.jpg"
-                }
+                src={getValidImage(hotel)}
                 alt="Hotel"
                 className="hotel-image"
               />
